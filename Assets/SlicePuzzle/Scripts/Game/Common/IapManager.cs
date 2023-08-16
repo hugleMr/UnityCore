6f31cb9b62d9c8228c042d5f909aff218be768b5
Harry <lehungcva@gmail.com>
2023-08-16 23:45:40 +0700

    fĩ

--- Assets/SlicePuzzle/Scripts/Game/Common/IapManager.cs
+++ Assets/SlicePuzzle/Scripts/Game/Common/IapManager.cs
@@ -2,7 +2,7 @@
 // This code can only be used under the standard Unity Asset Store End User License Agreement,
 // a copy of which is available at http://unity3d.com/company/legal/as_terms.
 
-#if CANDY_MATCH_ENABLE_IAP
+#if SLICE_PUZZLE_ENABLE_IAP
 using UnityEngine;
 using UnityEngine.Purchasing;
 
